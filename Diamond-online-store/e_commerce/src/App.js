82e099dcde4fe630a5a16b89a8c5af37c826276f
rehansaboor11879 <malikrehan11879@gmail.com>
2022-11-21 21:48:03 +0500

    create-repositry

---
+++ Diamond-online-store/e_commerce/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <a href="/">E-Commerce</a>
+      </header>
+      <main>List Product</main>
+    </div>
+  );
+};
+
+export default App;
